e47ca4bba62a14886bd85209a0d5a2fde9e9e9a3
Siddharth <sidgroks@gmail.com>
2018-02-22 13:57:14 +0530

    Move Up and Down done, add drag and drop

--- src/app.js
+++ src/app.js
@@ -3,94 +3,115 @@ import { render } from "react-dom";
 
 import { Container, Input, Panel } from "./modules/";
 
-
 class App extends React.Component {
-    //App State
-    state = {
-        todos: [],
+  //App State
+  state = {
+    todos: [],
+    todoText: ""
+  };
+
+  //Lifecycle Handlers
+  componentWillMount() {
+    var a = localStorage.getItem("todos");
+    if (a) {
+      this.setState({
+        todos: JSON.parse(a)
+      });
+    }
+  }
+
+  //Event Handlers
+  onInputChage = e => {
+    const val = e.target.value;
+    this.setState({
+      todoText: val
+    });
+  };
+
+  addToList = e => {
+    if (e.key === "Enter") {
+      if (!this.state.todoText) {
+        alert("Empty Task Field");
+        return;
+      }
+      const a = this.state.todos;
+      a.push({
+        id: this.state.todos.length,
+        value: this.state.todoText,
+        marked: false
+      });
+      this.setState({
+        todos: a,
         todoText: ""
+      });
+
+      localStorage.setItem("todos", JSON.stringify(a));
     }
+  };
 
-    //Lifecycle Handlers
-    componentWillMount(){
-        var a = localStorage.getItem('todos');
-        if(a){
-            this.setState({
-                todos:JSON.parse(a)
-            })
-        }
-    }
+  //Additional Functions
+  removeTodo = id => {
+    var a = this.state.todos;
+    a.splice(id, 1);
+    this.setState({
+      todos: a
+    });
+    localStorage.setItem("todos", JSON.stringify(a));
+  };
 
-    //Event Handlers
-    onInputChage = (e) => {
-        const val = e.target.value;
-        this.setState({
-            todoText: val
-        });
-    }
+  markTodo = id => {
+    var a = this.state.todos;
+    a[id].marked = true;
+    this.setState({
+      todos: a
+    });
+    localStorage.setItem("todos", JSON.stringify(a));
+  };
 
-    addToList=(e)=>{         
-        if(e.key==="Enter"){
-            if(!this.state.todoText){
-                alert("Empty Task Field");
-                return;   
-               }       
-            const a = this.state.todos;
-            a.push({
-                id:this.state.todos.length,
-                value:this.state.todoText,
-                marked:false
-            });
-            this.setState({
-                todos:a,
-                todoText:''
-            })
+  unmarkTodo = id => {
+    var a = this.state.todos;
+    a[id].marked = false;
+    this.setState({
+      todos: a
+    });
+    localStorage.setItem("todos", JSON.stringify(a));
+  };
 
-            localStorage.setItem('todos',JSON.stringify(a));
-        }
-    }
+  moveTodoUp = id => {
+    const todos = [...this.state.todos];
+    const currTodo = todos[id];
+    const toMoveTodo = todos[id - 1];
+    todos[id - 1] = currTodo;
+    todos[id] = toMoveTodo;
+    console.log(todos);
+    this.setState({ todos });
+  };
 
-    //Additional Functions
-    removeTodo=(id)=>{
-        var a = this.state.todos;
-        a.splice(id,1);
-        this.setState({
-            todos:a
-        });
-        localStorage.setItem('todos',JSON.stringify(a));
-    }
+  moveTodoDown = id => {};
 
-    markTodo=(id)=>{
-        var a = this.state.todos;
-        a[id].marked=true;
-        this.setState({
-            todos:a
-        });
-        localStorage.setItem('todos',JSON.stringify(a));
-    }
-
-    unmarkTodo=(id)=>{
-        var a = this.state.todos;
-        a[id].marked=false;
-        this.setState({
-            todos:a
-        });
-        localStorage.setItem('todos',JSON.stringify(a));
-    }
-
-    //Render Method
-    render() {
-        return (
-            <div id="app">
-                <Container>
-                    <Input elid="todo-text" onChange={this.onInputChage} keyPress={this.addToList} value={this.state.todoText} placeholder="Enter a task... " />
-                    <Panel dataList={this.state.todos} unMark={this.unmarkTodo} mark={this.markTodo} remove={this.removeTodo} />
-                </Container>
-            </div>
-        );
-    }
-
-
+  //Render Method
+  render() {
+    return (
+      <div id="app">
+        <Container>
+          <Input
+            elid="todo-text"
+            onChange={this.onInputChage}
+            keyPress={this.addToList}
+            value={this.state.todoText}
+            placeholder="Enter a task... "
+          />
+          <Panel
+            dataList={this.state.todos}
+            unMark={this.unmarkTodo}
+            mark={this.markTodo}
+            changePosition={{ up: this.moveTodoUp, down: this.moveTodoDown }}
+            remove={this.removeTodo}
+          />
+        </Container>
+      </div>
+    );
+  }
 }
 
-render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById("root"));
--- src/modules/panel.js
+++ src/modules/panel.js
@@ -3,27 +3,35 @@ import Button from "./button";
 import Text from "./text";
 
 export default function Panel(props) {
-    const listOfTextElms = props.dataList.map((item,index) =>
-        <div key={index} className="panel">
-            <Text value={item.value} slice={item.marked}/>
-            <div className="btn-container">
-            {!item.marked
-            ?<Button onClick={()=>props.mark(index)}>
-                <span className="oi" data-glyph="check"></span>
-            </Button>
-            :<Button onClick={()=>props.unMark(index)}>
-                <span className="oi" data-glyph="x"></span>
-            </Button>
-            }
-            <Button onClick={()=>props.remove(index)}>
-                <span className="oi" data-glyph="trash"></span>
-            </Button>
-            </div>
-        </div>
-    );
-    return (
-        <div className="panel-container">
-                {listOfTextElms}
-        </div>
-    );
+  const len = props.dataList.length;
+  const listOfTextElms = props.dataList.map((item, index) => (
+    <div key={item.id} className="panel">
+      <Text value={item.value} slice={item.marked} />
+      <div className="btn-container">
+        {index !== 0 ? (
+          <Button onClick={() => props.changePosition.up(index)}>
+            <span className="oi" data-glyph="arrow-thick-top" />
+          </Button>
+        ) : null}
+        {index == len-1 ? null : (
+          <Button onClick={() => props.changePosition.down(index)}>
+            <span className="oi" data-glyph="arrow-thick-bottom" />
+          </Button>
+        )}
+        {!item.marked ? (
+          <Button onClick={() => props.mark(index)}>
+            <span className="oi" data-glyph="check" />
+          </Button>
+        ) : (
+          <Button onClick={() => props.unMark(index)}>
+            <span className="oi" data-glyph="x" />
+          </Button>
+        )}
+        <Button onClick={() => props.remove(index)}>
+          <span className="oi" data-glyph="trash" />
+        </Button>
+      </div>
+    </div>
+  ));
+  return <div className="panel-container">{listOfTextElms}</div>;
 }
